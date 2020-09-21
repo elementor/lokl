cb11596bb1406e192724b5e08d0641fcc8c1c938
Leon Stafford <me@ljs.dev>
2020-09-22 03:40:50 +0930

    flip flop on using site name

--- scripts/backup_site.sh
+++ scripts/backup_site.sh
@@ -3,21 +3,18 @@
 cd /usr/html/ || exit 1
 
 # rm previous backups
-rm -f "/tmp/LOKL_DATABASE_BACKUP.sql"
-rm -f "/tmp/LOKL_DATABASE_BACKUP.sql.tar.gz"
+rm -f "/usr/LOKL_DATABASE_BACKUP.sql"
 rm -f "/tmp/LOKL_SITE_BACKUP.tar.gz" 
 
 # dump DB to tmp dir
-mysqldump -u root -pbanana wordpress > "/tmp/LOKL_DATABASE_BACKUP.sql"
-
-# compress DB dump
-tar cfz "/tmp/LOKL_DATABASE_BACKUP.sql.tar.gz" "/tmp/LOKL_DATABASE_BACKUP.sql"
-
-# rm uncompressed DB dump
-rm "/tmp/LOKL_DATABASE_BACKUP.sql"
+mysqldump -u root -pbanana wordpress > "/usr/LOKL_DATABASE_BACKUP.sql"
 
 cd /usr/ || exit 1
 
+echo "generating backup for site named $N"
+
 # generate archive of whole html dir and compresssed DB dump
-tar cfz "/tmp/LOKL_SITE_BACKUP.tar.gz" html /tmp/LOKL_DATABASE_BACKUP.sql.tar.gz
+tar cfz "/tmp/${N}_SITE_BACKUP.tar.gz" html LOKL_DATABASE_BACKUP.sql
+
+ls /tmp
 
