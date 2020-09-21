67a0c64864807afc47eeee1ca55ca8e0405aa5ce
Leon Stafford <me@ljs.dev>
2020-09-22 03:28:02 +0930

    simplify without env var

--- scripts/backup_site.sh
+++ scripts/backup_site.sh
@@ -2,14 +2,22 @@
 
 cd /usr/html/ || exit 1
 
-# backup DB to parent dir
-mysqldump -u root -pbanana wordpress > "../$N_DATABASE_BACKUP.sql"
-tar cfz "../$N_DATABASE_BACKUP.sql.tar.gz" "../$N_DATABASE_BACKUP.sql"
-rm "../$N_DATABASE_BACKUP.sql"
+# rm previous backups
+rm -f "/tmp/LOKL_DATABASE_BACKUP.sql"
+rm -f "/tmp/LOKL_DATABASE_BACKUP.sql.tar.gz"
+rm -f "/tmp/LOKL_SITE_BACKUP.tar.gz" 
 
-# backup whole html dir
+# dump DB to tmp dir
+mysqldump -u root -pbanana wordpress > "/tmp/LOKL_DATABASE_BACKUP.sql"
+
+# compress DB dump
+tar cfz "/tmp/LOKL_DATABASE_BACKUP.sql.tar.gz" "/tmp/LOKL_DATABASE_BACKUP.sql"
+
+# rm uncompressed DB dump
+rm "/tmp/LOKL_DATABASE_BACKUP.sql"
 
 cd /usr/ || exit 1
-tar cfz "/tmp/$N_SITE_BACKUP.tar.gz" html
 
-rm "../$N_DATABASE_BACKUP.sql.tar.gz"
+# generate archive of whole html dir and compresssed DB dump
+tar cfz "/tmp/LOKL_SITE_BACKUP.tar.gz" html /tmp/LOKL_DATABASE_BACKUP.sql.tar.gz
+
