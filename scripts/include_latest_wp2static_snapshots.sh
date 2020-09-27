97a272f83a5d8b7d0cc2246a3a4e85009c88f0c7
Leon Stafford <me@ljs.dev>
2020-09-28 06:55:04 +0930

    latest wp2static's into image builder

--- scripts/include_latest_wp2static_snapshots.sh
+++ scripts/include_latest_wp2static_snapshots.sh
@@ -10,29 +10,58 @@
 #      leonstafford/wp2static-addon-s3
 #      ...
 
+WP2STATIC_REPOS="leonstafford/wp2static
+leonstafford/wp2static-addon-s3
+leonstafford/wp2static-addon-zip
+leonstafford/wp2static-addon-wp2static-addon-advanced-crawling
+leonstafford/wp2static-addon-cloudflare-workers
+"
+
+# cleanup existing temp repos
+rm -Rf /tmp/wp2staticloklbuildcache
+mkdir -p /tmp/wp2staticloklbuildcache
+
+cd /tmp/wp2staticloklbuildcache || exit 1
+
 # iterate each repo
+for REPO in $WP2STATIC_REPOS
+do
+  DIRNAME="$(echo $REPO | cut -d\/ -f2)"
+  echo "Processing $DIRNAME..."
 
-# clone --depth=1
+  # clone --depth=1
+  git clone --depth=1 "git@github.com:$REPO.git"
 
-# cd into repo
+  # cd into repo
+  cd "$DIRNAME"
 
-# composer i
+  # composer test && composer coverage
+  # exit if non-zero
+  composer i -q || exit 1
+  composer test || exit 1
+  composer coverage || echo "No coverage command for this repo\n"
 
-# composer test && composer coverage
+  # rm ~/Downloads/wp2static-addon-s3.zip
+  rm -f "$HOME/Downloads/$DIRNAME.zip"
 
-# exit if non-zero
+  # composer build wp2static-addon-s3 (generates ~/Downloads/wp2static-addon-s3.zip)
+  composer build "$DIRNAME" || exit 1
 
-# rm ~/Downloads/wp2static-addon-s3.zip
+  # rm existing versioned WP2Static plugins from Lokl image builder
+  cp "$HOME/Downloads/$DIRNAME.zip" "$HOME/lokl/installers/default_plugins/$DIRNAME.zip"
 
-# composer build wp2static-addon-s3 (generates ~/Downloads/wp2static-addon-s3.zip)
+  # cp installer to ./installers/default_plugins 
+  cp "$HOME/Downloads/$DIRNAME.zip" "$HOME/lokl/installers/default_plugins/"
 
-# cp installer to ./installers/default_plugins 
+  # adjust run script to use ./installers/default_plugins/activate vs install_only
 
-# adjust run script to use ./installers/default_plugins/activate vs install_only
+  # run script will install and activate all wp2static plugins (or die trying)
 
-# run script will install and activate all wp2static plugins (or die trying)
+  # commit message on success: "updated WP2Static core and add-on snapshots"
+  #  "add to commit details the repo name and ref hash"
 
-# commit message on success: "updated WP2Static core and add-on snapshots"
-#  "add to commit details the repo name and ref hash"
+
+done
+
 
 
