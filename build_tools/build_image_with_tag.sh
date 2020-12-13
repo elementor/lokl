a778e5be019c97a8a835a2d0d29d4061d1b77cc5
Leon <me@ljs.dev>
2020-12-13 15:09:35 +1100

    note on build script

--- build_tools/build_image_with_tag.sh
+++ build_tools/build_image_with_tag.sh
@@ -2,6 +2,6 @@
 
 LOKL_VERSION="$1"
 
-# docker build -t lokl/lokl:"$LOKL_VERSION" .
+# TODO: check the --force-rm likely causing old containers to disappear 
 docker build -f php8/Dockerfile -t lokl/lokl:"$LOKL_VERSION" . --force-rm --no-cache
 
