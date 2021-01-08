4f72d83e7c35af3e2bccfa487ae8a13539333678
Leon <me@ljs.dev>
2021-01-08 23:31:01 +1000

    re #20 wip starting e2e tests

--- spec/lokl_spec.sh
+++ spec/lokl_spec.sh
@@ -6,7 +6,20 @@ Describe 'Default Lokl website'
       
       configure_wp2static() {
         # use spec helper for common commands
-        create_lokl_site
+        # create_lokl_site
+
+        # run new container
+        export lokl_php_ver=php8 
+        export lokl_site_name=lokltestsite
+        export lokl_site_port=4444
+
+        echo 'something ' >> /tmp/testlog
+
+        env | grep lokl_ >> /tmp/testlog
+
+        wget 'https://raw.githubusercontent.com/leonstafford/lokl-cli/master/cli.sh'
+
+        sh cli.sh 
 
         docker exec -it lokltestsite sh -c \
           "wp wp2static option set deployment_method zip && wp wp2static detect && wp wp2static crawl && \
@@ -14,13 +27,15 @@ Describe 'Default Lokl website'
       }
       
       zip_file_present() {
-        wget http://localhost:4321/wp-content/uploads/wp2static-processed-site.zip
+        # wget http://localhost:4444/wp-content/uploads/wp2static-processed-site.zip
         # do some checks on the zip (size, num files, index content?)  
+        echo ""
+        return 0
       }
       
-     When run configure_wp2static
+     When call configure_wp2static
      The output should include 'something'
-     The result of function zip_file_present should be true
+     The result of function zip_file_present should be success
     End
   End
 End
