dcd2a4610d0b2eaca739d8dac75dcb0c9ad6bfb6
Leon <me@ljs.dev>
2021-01-13 16:57:33 +1000

    print env info for release notes

--- build_tools/print_container_env_info.sh
+++ build_tools/print_container_env_info.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # TODO: accept arg for container name
+CONTAINER_TO_INSPECT="$1"
 
 echo "###############################################################"
 echo ""
@@ -12,10 +13,32 @@ echo "###############################################################"
 
 
 read -r -d '' PRINT_INFO_SCRIPT <<'ENDSCRIPT'
-  wp cli info && wp plugin list && wp config list | head -n 10
+  echo "" && \
+  echo "### WordPress version:" && \
+  echo "" && \
+  wp core version && \
+  echo "" && \
+  echo "### MariaDB (MySQL) version:" && \
+  echo "" && \
+  mysql --version && \
+  echo "" && \
+  echo "### WordPress environment, plugins and DB credentials:" && \
+  echo "" && \
+  wp cli info && wp plugin list && wp config list | head -n 10 && \
+  echo "" && \
+  echo "### PHPMyAdmin version:" && \
+  echo "" && \
+  wget -q "localhost:$P/phpmyadmin" -O - | grep 'Version' && \
+  echo "" && \
+  echo "### Enabled PHP features:" && \
+  echo "" && \
+  php -i | grep enabled && \
+  echo "" && \
+  echo "### Installed Alpine packages:" && \
+  echo "" && \
+  apk info
+  echo ""
 ENDSCRIPT
 
-echo $PRINT_INFO_SCRIPT
-
 # print OS, PHP and WordPress information
-docker exec -it new7testlocally /bin/sh -c "$PRINT_INFO_SCRIPT"
+docker exec -it "$CONTAINER_TO_INSPECT" /bin/sh -c "$PRINT_INFO_SCRIPT"
