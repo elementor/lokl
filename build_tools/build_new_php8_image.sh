e4bbf1336d698e4f9375eb9eb4c8760fd7154988
Leon Stafford <me@ljs.dev>
2021-06-09 09:43:58 +0930

    prep next version;wip xdebug add

--- build_tools/build_new_php8_image.sh
+++ build_tools/build_new_php8_image.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-LOKL_RELEASE_VERSION="5.0.0"
+LOKL_RELEASE_VERSION="5.0.1-dev"
 
 docker build -f php8/Dockerfile -t lokl/lokl:php8base . --force-rm --no-cache
 docker rm --force php8base
--- build_tools/push_to_docker_registry.sh
+++ build_tools/push_to_docker_registry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-LOKL_RELEASE_VERSION="5.0.0"
+LOKL_RELEASE_VERSION="5.0.1-dev"
 
 # PHP 8
 docker push "lokl/lokl:php8-$LOKL_RELEASE_VERSION"
